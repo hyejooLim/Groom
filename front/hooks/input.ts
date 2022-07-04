491639e56eda974b52393b421b109ec4ba009e6a
hyejooLim <lhyejoo@naver.com>
2022-07-04 19:20:37 +0900

    useInput > ReturnType에 TextAreaElement 추가

--- front/hooks/input.ts
+++ front/hooks/input.ts
@@ -1,10 +1,14 @@
 import React, { useState, useCallback, ChangeEvent, Dispatch } from 'react';
 
-type ReturnType<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<React.SetStateAction<T>>];
+type ReturnType<T> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  Dispatch<React.SetStateAction<T>>
+];
 
-const useInput = <T,>(initialValue: T): ReturnType<T> => {
+const useInput = <T>(initialValue: T): ReturnType<T> => {
   const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setValue(e.target.value as unknown as T);
   }, []);
 
