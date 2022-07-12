4e5cfa168c702f147c8775c330f952a51cfae413
hyejooLim <lhyejoo@naver.com>
2022-07-12 16:02:42 +0900

    add: 관리 페이지 > 로그아웃 버튼

--- front/components/ManageProfile.tsx
+++ front/components/ManageProfile.tsx
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Card } from 'antd';
+import { Button, Card } from 'antd';
 
 const StyledCard = styled(Card)`
+  .card_meta {
+    display: flex;
+    justify-content: space-between;
+
+    .logout_btn {
+      padding: 0;
+      width: 58px;
+      height: 24px;
+      font-size: 12px;
+      background-color: #fff;
+      color: #13a085;
+
+      :hover {
+        box-shadow: none;
+        border-color: #a5a5a5;
+      }
+    }
+  }
+
   .ant-card-body {
     padding: 18px;
 
@@ -19,13 +38,22 @@ const ManageProfile = () => {
     email: 'hong@naver.com',
   });
 
+  const handleLogout = () => {
+    // logout
+  };
+
   return (
     <StyledCard
       cover={
         <img alt='example' src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png' width={214} height={200} />
       }
     >
-      <Card.Meta title={`${user.name}님`} description={user.email} />
+      <div className='card_meta'>
+        <Card.Meta title={`${user.name}님`} description={user.email} />
+        <Button className='logout_btn' onClick={handleLogout}>
+          로그아웃
+        </Button>
+      </div>
     </StyledCard>
   );
 };
