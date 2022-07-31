3b48e60fbe503dde438052c9c356da58ba075832
hyejooLim <lhyejoo@naver.com>
2022-07-31 23:44:12 +0900

    style: react-modal에 스타일 적용

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import 'antd/dist/antd.css';
 import '../styles/global.css';
+import '../styles/modal.css';
 
 const App = ({ Component }) => {
   return (
