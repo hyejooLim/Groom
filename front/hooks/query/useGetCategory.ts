9746fb8d6769ee9d27a2689579f5a4f7e8b6b4c2
hyejooLim <lhyejoo@naver.com>
2022-08-27 20:16:46 +0900

    query key를 다르게 주어 캐싱을 별도로 관리

--- front/hooks/query/useGetCategory.ts
+++ front/hooks/query/useGetCategory.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import getCategory from '../../apis/getCategory';
 
 const useGetCategory = (name: string) =>
-  useQuery(['category'], () => getCategory(name), {
+  useQuery(['category', name], () => getCategory(name), {
     onSuccess: (data) => {
       console.log('category', data);
     },
