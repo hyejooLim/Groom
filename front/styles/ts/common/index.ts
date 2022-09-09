adbfa6134d0aba18940f3aa68812382db453524e
hyejooLim <lhyejoo@naver.com>
2022-09-09 16:45:51 +0900

    style: inline css -> styled-components로 수정

--- front/styles/ts/common/index.ts
+++ front/styles/ts/common/index.ts
@@ -1,6 +1,26 @@
 import styled from 'styled-components';
 import { Button } from 'antd';
 
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  & .text {
+    font-size: 18px;
+  }
+
+  & .title {
+    color: #ff5544;
+    margin: 0 6px 0 8px;
+  }
+
+  & .count {
+    font-size: 14px;
+    color: #888;
+    margin-left: 8px;
+  }
+`;
+
 export const CloseButton = styled(Button)`
   font-size: 20px;
   background-color: transparent;
