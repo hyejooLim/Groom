15c126dddc7f8f0022d56f8cc46fb18ed538fb57
hyejooLim <lhyejoo@naver.com>
2022-10-19 17:05:26 +0900

    메인 페이지 > useGetPosts로 전체 글 로드하도록 수정

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
 
 import AppLayout from '../components/layouts/AppLayout';
 import Title from '../components/common/Title';
 import PostList from '../components/post/PostList';
-import { mainPostsState } from '../recoil/posts';
+import { useGetPosts } from '../hooks/query/posts';
 
 const Home = () => {
-  const mainPosts = useRecoilValue(mainPostsState);
+  const { data: posts } = useGetPosts();
 
   return (
     <AppLayout>
       <div style={{ textAlign: 'center' }}>
         <Title title='전체 글' />
       </div>
-      <PostList posts={mainPosts} />
+      <PostList posts={posts} />
     </AppLayout>
   );
 };
