1d516dd20ac5d8fcf23b91a0ddb27d76c1956016
hyejooLim <lhyejoo@naver.com>
2022-10-20 16:54:00 +0900

    rename: subscribedPostManage -> subscribedPosts

--- front/apis/search/searchSubscribedPosts.ts
+++ front/apis/search/searchSubscribedPosts.ts
@@ -2,7 +2,7 @@ import clientApi from '..';
 import { PostItem } from '../../types';
 
 const searchSubscribedPosts = async (keyword: string, searchType: string): Promise<PostItem[]> => {
-  const response = await clientApi.get<PostItem[]>(`/search/${keyword}/${searchType}/subscribedPostManage`);
+  const response = await clientApi.get<PostItem[]>(`/search/${keyword}/${searchType}/subscribedPosts`);
 
   return response;
 };
