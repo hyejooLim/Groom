6d087e13f8fe18f3b9ef6f5c1f0487b159b7de69
hyejooLim <lhyejoo@naver.com>
2022-10-20 16:53:31 +0900

    rename: postManage -> posts

--- front/apis/search/searchPosts.ts
+++ front/apis/search/searchPosts.ts
@@ -2,7 +2,7 @@ import clientApi from '..';
 import { PostItem } from '../../types';
 
 const searchPosts = async (keyword: string, searchType: string): Promise<PostItem[]> => {
-  const response = await clientApi.get<PostItem[]>(`/search/${keyword}/${searchType}/postManage`);
+  const response = await clientApi.get<PostItem[]>(`/search/${keyword}/${searchType}/posts`);
 
   return response;
 };
