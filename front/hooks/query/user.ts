2806bdb28887db8b5348175146b7f0a302058a5a
hyejooLim <lhyejoo@naver.com>
2022-10-24 17:17:47 +0900

    refactor: 구독 글 관리 페이지 관련 컴포넌트 subscribedPosts.tsx 하나에 작성

--- front/hooks/query/user.ts
+++ front/hooks/query/user.ts
@@ -2,22 +2,15 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useSetRecoilState } from 'recoil';
 
 import { userState } from '../../recoil/user';
-import { managePostsState } from '../../recoil/manage';
-import { manageSubscribedPostsState } from '../../recoil/manage';
 import getUser from '../../apis/user/getUser';
 import updateUser from '../../apis/user/updateUser';
 
 const useGetUser = () => {
   const setUser = useSetRecoilState(userState);
-  const setManagePosts = useSetRecoilState(managePostsState);
-  const setManageSubscribedPosts = useSetRecoilState(manageSubscribedPostsState);
 
   return useQuery(['user'], getUser, {
     onSuccess: (data) => {
       setUser(data);
-      // setManagePosts(data?.posts);
-      // setManageSubscribedPosts(data?.subscribedPosts);
-
       console.log('user', data);
     },
     refetchOnWindowFocus: false,
---
+++ front/pages/manage/subscribedPosts.tsx
@@ -0,0 +1,124 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import Head from 'next/head';
+import Router, { useRouter } from 'next/router';
+import Link from 'next/link';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { CloseCircleOutlined } from '@ant-design/icons';
+
+import ManageLayout from '../../components/layouts/ManageLayout';
+import PostManageList from '../../components/manage/PostManageList';
+import PaginationContainer from '../../components/common/PaginationContainer';
+import SearchInput from '../../components/manage/SearchInput';
+import { MANAGE_PAGE_SIZE } from '../../recoil/page';
+import { manageSubscribedPostsState } from '../../recoil/manage';
+import { useGetUserSubscribedPosts } from '../../hooks/query/posts';
+import { useSearchCategoryOnUserSubscribedPosts, useSearchUserSubscribedPosts } from '../../hooks/query/search';
+import { TitleWrapper, CloseButton } from '../../styles/ts/common';
+import { useGetUser } from '../../hooks/query/user';
+import { userState } from '../../recoil/user';
+
+const ManageSubscribedPosts = () => {
+  const router = useRouter();
+  const { category: categoryId, searchKeyword, searchType } = router.query;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [firstIndex, setFirstIndex] = useState(0);
+  const [lastIndex, setLastIndex] = useState(MANAGE_PAGE_SIZE);
+
+  const { refetch } = useGetUserSubscribedPosts();
+  const { data: category } = useSearchCategoryOnUserSubscribedPosts(Number(categoryId));
+  useSearchUserSubscribedPosts(String(searchKeyword), String(searchType));
+
+  const manageSubscribedPosts = useRecoilValue(manageSubscribedPostsState);
+
+  const onInitPage = () => {
+    setCurrentPage(1);
+    setFirstIndex(0);
+    setLastIndex(MANAGE_PAGE_SIZE);
+  };
+
+  useEffect(() => {
+    onInitPage();
+
+    if (Object.keys(router.query).length === 0) {
+      refetch();
+    }
+  }, [router.query]);
+
+  const onChangePage = useCallback(
+    (page: number) => {
+      setCurrentPage(page);
+      setFirstIndex((page - 1) * MANAGE_PAGE_SIZE);
+      setLastIndex(page * MANAGE_PAGE_SIZE);
+    },
+    [MANAGE_PAGE_SIZE]
+  );
+
+  const onSearchInput = (keyword: string, searchType: string) => {
+    onInitPage();
+    Router.push({ pathname: '/manage/subscribedPosts', query: { searchKeyword: keyword, searchType } });
+  };
+
+  const onClickCategory = (id: number) => {
+    onInitPage();
+    Router.push({ pathname: '/manage/subscribedPosts', query: { category: id } });
+  };
+
+  return (
+    <ManageLayout>
+      <Head>
+        <title>Groom | 구독 글 관리</title>
+        {searchKeyword && <title>Groom | 구독 글 관리 '{searchKeyword}'의 검색결과</title>}
+        {categoryId && <title>Groom | 구독 글 관리 '{category?.name}' 카테고리의 글 목록</title>}
+      </Head>
+      <div style={{ marginTop: -20 }}>
+        <TitleWrapper>
+          {Object.keys(router.query).length === 0 ? (
+            <>
+              <span className='text'>구독 글 관리</span>
+              <span className='count'>{manageSubscribedPosts?.length}</span>
+            </>
+          ) : (
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Link href='/manage/subscribedPosts'>
+                <a>
+                  <CloseButton>
+                    <CloseCircleOutlined />
+                  </CloseButton>
+                </a>
+              </Link>
+              {searchKeyword && (
+                <>
+                  <span className='text title'>'{searchKeyword}'</span>
+                  <span className='text'>검색결과</span>
+                </>
+              )}
+              {categoryId && (
+                <>
+                  <span className='text title'>'{category?.name}'</span>
+                  <span className='text'>글</span>
+                </>
+              )}
+              <span className='count'>{manageSubscribedPosts?.length}</span>
+            </div>
+          )}
+        </TitleWrapper>
+        <SearchInput placeholder='구독 글' onSearch={onSearchInput} />
+        <PostManageList
+          posts={manageSubscribedPosts}
+          firstIndex={firstIndex}
+          lastIndex={lastIndex}
+          onClickCategory={onClickCategory}
+        />
+      </div>
+      <PaginationContainer
+        pageSize={MANAGE_PAGE_SIZE}
+        current={currentPage}
+        total={manageSubscribedPosts?.length}
+        onChange={onChangePage}
+      />
+    </ManageLayout>
+  );
+};
+
+export default ManageSubscribedPosts;
