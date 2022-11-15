1a02d87826c4a94b909d576a91dd0bf9ff9637a3
hyejooLim <lhyejoo@naver.com>
2022-11-15 15:18:02 +0900

    remove: 사용하지 않는 atom 제거

--- front/hooks/query/visitorsCount.ts
+++ front/hooks/query/visitorsCount.ts
@@ -1,22 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import { useSetRecoilState } from 'recoil';
-
 import getVisitorsCount from '../../apis/count';
-import { todayCountState, totalCountState } from '../../recoil/count';
 
-const useGetVisitorsCount = () => {
-  const setTodayCount = useSetRecoilState(todayCountState);
-  const setTotalCount = useSetRecoilState(totalCountState);
-
-  return useQuery(['visitorsCount'], () => getVisitorsCount(), {
+const useGetVisitorsCount = () =>
+  useQuery(['visitorsCount'], getVisitorsCount, {
     onSuccess: (data) => {
-      setTodayCount(data?.todayCount);
-      setTotalCount(data?.totalCount);
       console.log('visitorsCount', data);
     },
     refetchInterval: 10000,
     refetchOnWindowFocus: false,
   });
-};
 
 export default useGetVisitorsCount;
