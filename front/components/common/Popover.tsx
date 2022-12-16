797923b452de65765491c87e765bfdf0251658cf
hyejooLim <lhyejoo@naver.com>
2022-12-17 00:27:59 +0900

    refactor: Popover > 스타일 관련 코드 styles 폴더에 정리

--- front/components/common/Popover.tsx
+++ front/components/common/Popover.tsx
@@ -1,62 +1,7 @@
 import React, { FC, useCallback, useEffect, useRef } from 'react';
-import styled from 'styled-components';
 import classNames from 'classnames';
 
-const PropoverWrapper = styled.div`
-  position: absolute;
-  background-color: #fff;
-  padding: 10px 0;
-  box-shadow: 0 0 0 1px rgb(0 0 0 / 10%), 0 2px 5px rgb(0 0 0 / 10%);
-
-  &.isShow {
-    visibility: visible;
-  }
-
-  &:not(.isShow) {
-    visibility: hidden;
-  }
-
-  &::before {
-    content: '';
-    border-top: 6px solid #fff;
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    position: absolute;
-    left: 13px;
-    top: 116px;
-    z-index: 2;
-  }
-
-  &::after {
-    content: '';
-    border-top: 6px solid #999;
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    position: absolute;
-    left: 13px;
-    top: 117px;
-  }
-
-  & div {
-    width: 100%;
-    padding: 7px 18px;
-    font-family: 'Noto Sans DemiLight', AppleSDGothicNeo, 'Malgun Gothic', '맑은 고딕', '돋움', dotum, sans-serif !important;
-    line-height: 16px;
-    color: #333;
-    text-align: left;
-    box-sizing: border-box;
-    text-decoration: none;
-
-    &:hover {
-      background-color: #f5f5f5;
-      cursor: pointer;
-    }
-
-    .icon {
-      margin-right: 8px;
-    }
-  }
-`;
+import { PopoverWrapper } from '../../styles/ts/components/common/Popover';
 
 interface PopoverProps {
   isShow: boolean;
@@ -90,9 +35,9 @@ const Popover: FC<PopoverProps> = ({ isShow, onClose, children }) => {
   }, [isShow]);
 
   return (
-    <PropoverWrapper ref={popoverRef} className={classNames('popover', { isShow })} onClick={onClose}>
+    <PopoverWrapper ref={popoverRef} className={classNames('popover', { isShow })} onClick={onClose}>
       <span onClick={(e) => e.stopPropagation()}>{children}</span>
-    </PropoverWrapper>
+    </PopoverWrapper>
   );
 };
 
---
+++ front/styles/ts/components/common/Popover.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const PopoverWrapper = styled.div`
+  position: absolute;
+  background-color: #fff;
+  padding: 10px 0;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 10%), 0 2px 5px rgb(0 0 0 / 10%);
+
+  &.isShow {
+    visibility: visible;
+  }
+
+  &:not(.isShow) {
+    visibility: hidden;
+  }
+
+  &::before {
+    content: '';
+    border-top: 6px solid #fff;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    position: absolute;
+    left: 13px;
+    top: 116px;
+    z-index: 2;
+  }
+
+  &::after {
+    content: '';
+    border-top: 6px solid #999;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    position: absolute;
+    left: 13px;
+    top: 117px;
+  }
+
+  & div {
+    width: 100%;
+    padding: 7px 18px;
+    font-family: 'Noto Sans DemiLight', AppleSDGothicNeo, 'Malgun Gothic', '맑은 고딕', '돋움', dotum, sans-serif !important;
+    line-height: 16px;
+    color: #333;
+    text-align: left;
+    box-sizing: border-box;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #f5f5f5;
+      cursor: pointer;
+    }
+
+    .icon {
+      margin-right: 8px;
+    }
+  }
+`;
