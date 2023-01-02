d91800b8598a26aa6955cb228ff2b7d413c835f7
hyejooLim <lhyejoo@naver.com>
2023-01-02 17:09:21 +0900

    remove: 불필요한 코드 삭제

--- front/hooks/query/user.ts
+++ front/hooks/query/user.ts
@@ -1,17 +1,9 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { useSetRecoilState } from 'recoil';
-
-import { userState } from '../../recoil/user';
 import getUser from '../../apis/user/getUser';
 import updateUser from '../../apis/user/updateUser';
 
 const useGetUser = () => {
-  const setUser = useSetRecoilState(userState);
-
   return useQuery(['user'], getUser, {
-    onSuccess: (data) => {
-      setUser(data);
-    },
     refetchOnWindowFocus: false,
     staleTime: Infinity,
   });
--- front/recoil/user.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import { UserType } from '../types';
-
-export const userState = atom<UserType>({
-  key: 'user',
-  default: null,
-});
