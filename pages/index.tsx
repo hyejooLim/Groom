1bd0e47536a56db2e25ad702c76df564d2fc930b
hyejooLim <lhyejoo@naver.com>
2023-03-29 23:24:12 +0900

    test: getStaticProps without revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       dehydratedState: dehydrate(queryClient),
     },
-    revalidate: 10,
+    // revalidate: 60,
   };
 };
 
