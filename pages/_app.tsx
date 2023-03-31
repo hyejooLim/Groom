af1384270a5aef07a5eac0f0be11762b672bf5de
hyejooLim <lhyejoo@naver.com>
2023-03-31 18:20:54 +0900

    test: react-query-devtools 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider, QueryClient, Hydrate } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import 'antd/dist/antd.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,8 +12,20 @@ import '../styles/css/global.css';
 import '../styles/css/modal.css';
 import '../styles/css/Skeleton.css';
 
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then((d) => ({
+    default: d.ReactQueryDevtools,
+  }))
+);
+
 const App = ({ Component, pageProps }) => {
   const [queryClient] = useState(() => new QueryClient());
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
 
   return (
     <>
@@ -26,6 +39,12 @@ const App = ({ Component, pageProps }) => {
               <Component {...pageProps} />
             </RecoilRoot>
           </Hydrate>
+          <ReactQueryDevtools initialIsOpen={true} position='bottom-right' />
+          {showDevtools && (
+            <React.Suspense fallback={null}>
+              <ReactQueryDevtoolsProduction />
+            </React.Suspense>
+          )}
         </QueryClientProvider>
       </SessionProvider>
     </>
