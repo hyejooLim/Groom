ae2abf5e4ef63514fd623c3261df105b6f55cc9f
hyejooLim <lhyejoo@naver.com>
2023-03-31 23:32:10 +0900

    fix: TypeError. Converting circular structure to JSON

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -2,12 +2,17 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.body.token !== process.env.NEXT_PUBLIC_MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res.status(401).send({ message: 'Invalid token' });
   }
 
   try {
+    if (req.body?.path === undefined) {
+      return res.status(400).send({ message: 'Bad request (no body)' });
+    }
+
     await res.revalidate(req.body.path);
-    return res.json({ revalidated: true });
+
+    return res.status(200).json({ revalidated: true });
   } catch (err) {
     return res.status(500).send('Error revalidating');
   }
