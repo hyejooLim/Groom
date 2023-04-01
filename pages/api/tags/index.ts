837071e572a82d92e4c619b930152898895d9948
hyejooLim <lhyejoo@naver.com>
2023-04-01 13:27:22 +0900

    modify: 모든 태그 받아오도록 수정

--- pages/api/tags/index.ts
+++ pages/api/tags/index.ts
@@ -4,17 +4,9 @@ import prisma from '../../../lib/prisma';
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
-      const tags = await prisma.tag.findMany({
-        select: {
-          id: true,
-          name: true,
-        },
-        orderBy: {
-          id: 'asc',
-        },
-      });
+      const tags = await prisma.tag.findMany({});
 
-      return res.status(200).json(tags);
+      res.status(200).json(tags);
     }
   } catch (err) {
     console.error(err);
