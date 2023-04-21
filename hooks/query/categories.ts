d24855cd0a86824c878cffcb8f7740ef9e04e3c6
hyejooLim <lhyejoo@naver.com>
2023-04-21 15:51:31 +0900

    delete: 불필요한 파일 삭제

--- hooks/query/categories.ts
+++ hooks/query/categories.ts
@@ -1,20 +1,12 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { useSetRecoilState } from 'recoil';
 
-import { categoriesState } from '../../recoil/categories';
 import getCategories from '../../apis/categories/getCategories';
 import updateCategories from '../../apis/categories/updateCategories';
 
-const useGetCategories = () => {
-  const setCategories = useSetRecoilState(categoriesState);
-
-  return useQuery(['categories'], getCategories, {
-    onSuccess: (data) => {
-      setCategories(data);
-    },
+const useGetCategories = () =>
+  useQuery(['categories'], getCategories, {
     refetchOnWindowFocus: false,
   });
-};
 
 const useUpdateCategories = () => {
   const queryClient = useQueryClient();
--- recoil/categories.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-import { CategoryItem } from '../types';
-
-export const categoriesState = atom<CategoryItem[]>({
-  key: 'categories',
-  default: [],
-  dangerouslyAllowMutability: true,
-});
