26cb966808ba0381b905a53cb87c105bce5cfc8a
hyejooLim <lhyejoo@naver.com>
2023-04-24 23:02:08 +0900

    modify: 메인 페이지 SSG -> SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { dehydrate, QueryClient } from '@tanstack/react-query';
 
@@ -15,7 +15,7 @@ import { useGetPosts, useGetPostsPerPage } from '../hooks/query/posts';
 const Home = () => {
   const router = useRouter();
   const { page } = router.query;
-  
+
   const { data: posts } = useGetPosts();
   const { data: postsByPage, isLoading } = useGetPostsPerPage(page ? Number(page) : -1);
 
@@ -29,8 +29,13 @@ const Home = () => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // const { req } = context;
+  // const session = await getSession({ req });
+  // const email = session && session?.user.email;
+
   const queryClient = new QueryClient();
+  context.res.setHeader('Cache-Control', 'public, s-maxage=31536000, max-age=59');
 
   await Promise.all([
     queryClient.prefetchQuery(['user'], getUser),
@@ -45,5 +50,24 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const queryClient = new QueryClient();
+//   const session = await getSession();
+//   console.log('session!!', session);
+
+//   await Promise.all([
+//     queryClient.prefetchQuery(['user'], getUser),
+//     queryClient.prefetchQuery(['posts'], getPosts),
+//     queryClient.prefetchQuery(['categories'], getCategories),
+//     queryClient.prefetchQuery(['visitorsCount'], getVisitorsCount),
+//   ]);
+
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
 
 export default Home;
