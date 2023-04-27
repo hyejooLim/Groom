e60486b4a1ed9a166bd06b941cc538404c871fed
hyejooLim <lhyejoo@naver.com>
2023-04-27 16:21:31 +0900

    feat: 카테고리 수정 시 포스트 페이지 revalidate

--- apis/categories/updateCategories.ts
+++ apis/categories/updateCategories.ts
@@ -1,5 +1,9 @@
+import { useRecoilValue } from 'recoil';
+
 import clientApi from '..';
 import { CategoryItem } from '../../types';
+import { revalidatePostPage } from '../revalidate';
+import { mainPostsState } from '../../recoil/posts';
 
 interface UpdateCategoriesProps {
   append: CategoryItem[];
@@ -7,8 +11,13 @@ interface UpdateCategoriesProps {
   delete: CategoryItem[];
 }
 
-const updateCategories = async ({ data }: { data: UpdateCategoriesProps }): Promise<Response> => {
-  return await clientApi.put('/categories', data);
+const updateCategories = async ({ data }: { data: UpdateCategoriesProps }) => {
+  const response = await clientApi.put('/categories', data);
+
+  if (response === 'ok') {
+    const posts = useRecoilValue(mainPostsState);
+    posts.map((post) => revalidatePostPage(post.id));
+  }
 };
 
 export default updateCategories;
