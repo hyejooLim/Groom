e16b3a77d565ee9db1f1c28b9908eb83f6b7b2d2
hyejooLim <lhyejoo@naver.com>
2026-08-16 00:33:39 +0900

    refactor: antd to mui (Editor > toolbar)

--- components/editor/EditorToobar.tsx
+++ components/editor/EditorToobar.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 import { signOut, useSession } from 'next-auth/react';
 import Router from 'next/router';
 import Image from 'next/image';
-import { Popover } from 'antd';
-import Avatar from '@mui/material/Avatar';
+import { Avatar, Button, Popover } from '@mui/material';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
-import * as S from '../../styles/ts/components/editor/EditorToolbar';
 import logo from '../../public/Groom_Logo_No_Background.png';
 
 const EditorToolbar = () => {
@@ -31,50 +29,67 @@ const EditorToolbar = () => {
   };
 
   const title = (
-    <div style={{ margin: '10px 5px' }}>
-      <p style={{ margin: 0, fontSize: '16px' }}>{session?.user.name}</p>
-      <p style={{ color: '#777', fontWeight: '300', fontSize: '13px' }}>{session?.user.email}</p>
+    <div className='my-2 mx-1'>
+      <p className='text-md'>{session?.user.name}</p>
+      <p className='text-grey font-light'>{session?.user.email}</p>
     </div>
   );
 
-  const content = <S.LogoutButton onClick={handleLogout}>로그아웃</S.LogoutButton>;
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(e.currentTarget);
+    handleToggleShowInfo?.();
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    handleToggleShowInfo?.();
+  };
+
+  const open = Boolean(anchorEl) || showInfo;
 
   return (
-    <S.Toolbar className='tool_bar'>
-      <S.ImageWrapper>
+    <div className='fixed top-0 z-1 h-[70px] w-full min-w-[944px] border-b border-black/[0.05] bg-white transition-opacity duration-500'>
+      <div className='absolute left-0 top-0 z-20 m-[8px_0_0_34px] border-0 bg-transparent p-0 outline-none max-[1060px]:hidden'>
         <Image
           src={logo}
           alt='groom_logo'
           width={140}
           height={60}
           onClick={handleRouteHome}
-          style={{ cursor: 'pointer' }}
+          className='cursor-pointer'
         />
-      </S.ImageWrapper>
-      <S.AuthorInfo>
+      </div>
+      <div className='fixed right-0 top-0 z-20 mr-5 mt-[18px] flex items-center max-[1060px]:hidden'>
         <span>{session?.user.name}</span>
-        <Popover
-          placement='bottomRight'
-          title={title}
-          content={content}
-          trigger='click'
-          visible={showInfo}
-          onVisibleChange={handleToggleShowInfo}
-        >
-          <div
-            style={{
-              margin: '0 34px 0 14px',
-              display: 'inline-block',
-              cursor: 'pointer',
-            }}
-          >
+        <div>
+          <div onClick={handleClick} className='mx-[34px] my-0 ml-[14px] inline-block cursor-pointer'>
             <Avatar sx={{ width: 32, height: 32 }}>
               <PersonOutlineOutlinedIcon />
             </Avatar>
           </div>
-        </Popover>
-      </S.AuthorInfo>
-    </S.Toolbar>
+          <Popover
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <div className='p-4 flex flex-col items-end'>
+              {title && <div className='font-bold'>{title}</div>}
+              <Button onClick={handleLogout}>로그아웃</Button>
+            </div>
+          </Popover>
+        </div>
+      </div>
+    </div>
   );
 };
 
